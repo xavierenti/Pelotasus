e987ba732b9f962adb33361ffc49d65b83908306
uri <oriol.ar4@gmail.com>
2022-04-05 15:46:58 +0200

    buguitos

--- Pelotasus/Assets/Scripts/PersonMove.cs
+++ Pelotasus/Assets/Scripts/PersonMove.cs
@@ -14,25 +14,13 @@ public class PersonMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FaceMouse();
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
-        if (Input.GetButtonDown("Jump")) && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
-            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse;
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
         }
     }
-    void FaceMouse()
-    {
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(MousePosition);
 
-        Vector2 direction = new Vector2(
-            mousePosition.x - transform.position.x,
-            mousePosition.y - transform.position.y
-        );
-
-        transform.up = direction;
-    }
 }
